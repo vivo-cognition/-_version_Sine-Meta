1ad3b8029786764bc9ac8b6d5f3bb10fea9257f3
Оля Милищук <olgamilisuk020@gmail.com>
2026-03-13 16:20:14 +0300

    Final clean version

--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
-#include <ostream>
-#include "MyString.h"
-
+#include <iostream>
+#include "ItemManager.h"
+#include <Windows.h>
+#include "Enum.h"
 int main() {
     SetConsoleOutputCP(1251);
     ItemManager manager("Items.txt");
 
-	MyString text1 = "Hell Maks!";
-	std::cout << "(text1 == text) = "<<(text1 == text) << std::endl;
-	std::cout << "(text == text1) = "<<(text == text1) << std::endl;
-	std::cout <<"(text == Hello word!) = "<< (text == "Hello word!") << std::endl;
-
-	text1 = text;
-	std::cout <<"text1 = " << text1 << std::endl;
-	text1 = "Hi Maks!";
-	std::cout<<"text1 = " << text1 << std::endl;
-
-	std::cout<<"text1[0] << text1[4] = " << text1[0] << text1[4] << std::endl;
-	std::cout <<"text.size() = " << text.size() << std::endl;
-	std::cout<< " text.c_str() = " << text.c_str() << std::endl;
-	text.clear();
-	std::cout<<"text.clear(); = " << text;
+    if (manager.loadItemsFromTXT()) {
+        std::cout << "--- УСПЕХ: Файл загружен успешно! ---" << std::endl;
+    }
+    else {
+        std::cerr << "--- ОШИБКА: Не удалось загрузить файл! ---" << std::endl;
+        return 1;
+    }
+    int total = manager.getTotalItemCount();
+    std::cout << "Всего предметов в базе: " << total << std::endl;
+    manager.printAllItems();
+    const ItemVector& boots = manager.getItemBySlot(SlotType::FEET);
+    std::cout << "Предметов в слоте FEET: " << boots.getSize() << std::endl;
+    if (boots.getSize() > 0) {
+        std::cout << "Первый предмет в сапогах: " << boots.getAt(0).getName().c_str() << std::endl;
+    }
+    std::cout << "\nНажмите Enter, чтобы выйти...";
+    std::cin.get();
 
     return 0;
 }
